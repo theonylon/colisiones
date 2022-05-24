d1a8792d2f3b5399119bd1635cdb726b4bd5e58e
theonlyone <102159023+theonylon@users.noreply.github.com>
2022-05-24 10:35:06 -0300

    helice game done

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if (Input.GetKeyDown(KeyCode.W))
+        {
+            
+                transform.position = new Vector3(-4f, 1.6f, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            
+                transform.position = new Vector3(-4f, 0.5f, 0);
+           
+        }
+    }
+}
